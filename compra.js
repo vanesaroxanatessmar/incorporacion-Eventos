cb511e43fd443c549a7f6a878dbd22586dc63d03
vane <vanesatessmar@gmail.com>
2022-10-28 21:38:54 -0300

    agregue funiones para poder vaciar el carrito completo

--- compra.js
+++ compra.js
@@ -1,139 +1,130 @@
-const Clickbutton = document.querySelectorAll('.button')
-const tbody = document.querySelector('.tbody')
-let carrito = []
+// Variables 
 
-Clickbutton.forEach(btn => {
-  btn.addEventListener('click', addToCarritoItem)
-})
+const carrito = document.querySelector("#carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
+const listaRopas = document.querySelector("#lista-ropas");
+let articulosCarrito = [];
 
+cargarEventListeners();
 
-function addToCarritoItem(e){
-  const button = e.target
-  const item = button.closest('.card')
-  const itemTitle = item.querySelector('.card-title').textContent;
-  const itemPrice = item.querySelector('.precio').textContent;
-  const itemImg = item.querySelector('.card-img-top').src;
-  
-  const newItem = {
-    title: itemTitle,
-    precio: itemPrice,
-    img: itemImg,
-    cantidad: 1
-  }
+function cargarEventListeners() {
+    //Agregar al carrito
+    listaRopas.addEventListener("click", agregarRopa);
 
-  addItemCarrito(newItem)
+    // Eliminar ropas del carrito
+    carrito.addEventListener("click", eliminarRopa);
+
+    // Vaciar el carrito
+    vaciarCarritoBtn.addEventListener("click", () => {
+        articulosCarrito = []; 
+
+        carritoHTML();  
+    })
+
+    //Muestra productos, local storage
+
+    document.addEventListener("DOMContentLoaded", () => {
+        articulosCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
+        carritoHTML();
+    });
+}
+
+//Funciones
+
+function agregarRopa(e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains("agregar-carrito")) {
+        const ropaSeleccionado = e.target.parentElement.parentElement;
+        leerDatosRopa(ropaSeleccionado);  
+    }
+}
+
+//Elimina producto del carrito
+
+function eliminarRopa(e) {
+    if (e.target.classList.contains("borrar-ropa")) {
+      const ropaId = e.target.getAttribute("data-id");  
+
+       // Elimina del arreglo de articulosCarrito por el data-id
+        articulosCarrito = articulosCarrito.filter( ropa => ropa.id !== ropaId)
+
+        carritoHTML(); 
+    }
 }
 
 
-function addItemCarrito(newItem){
+function leerDatosRopa(ropa) {
+    // console.log(ropa);
 
-  const alert = document.querySelector('.alert')
-
-  setTimeout( function(){
-    alert.classList.add('hide')
-  }, 2000)
-    alert.classList.remove('hide')
-
-  const InputElemnto = tbody.getElementsByClassName('input__elemento')
-  for(let i =0; i < carrito.length ; i++){
-    if(carrito[i].title.trim() === newItem.title.trim()){
-      carrito[i].cantidad ++;
-      const inputValue = InputElemnto[i]
-      inputValue.value++;
-      CarritoTotal()
-      return null;
+    const infoRopa = {
+        imagen: ropa.querySelector("img").src,
+        titulo: ropa.querySelector("h4").textContent,
+        precio: ropa.querySelector(".precio span").textContent,
+        id: ropa.querySelector("a").getAttribute("data-id"),
+        cantidad: 1,
     }
-  }
-  
-  carrito.push(newItem)
-  
-  renderCarrito()
-} 
-
-
-function renderCarrito(){
-  tbody.innerHTML = ''
-  carrito.map(item => {
-    const tr = document.createElement('tr')
-    tr.classList.add('ItemCarrito')
-    const Content = `
     
-    <th scope="row">1</th>
-            <td class="table__productos">
-              <img src=${item.img}  alt="">
-              <h6 class="title">${item.title}</h6>
-            </td>
-            <td class="table__price"><p>${item.precio}</p></td>
-            <td class="table__cantidad">
-              <input type="number" min="1" value=${item.cantidad} class="input__elemento">
-              <button class="delete btn btn-danger">x</button>
-            </td>
-    
-    `
-    tr.innerHTML = Content;
-    tbody.append(tr)
-
-    tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
-    tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
-  })
-  CarritoTotal()
-}
-
-function CarritoTotal(){
-  let Total = 0;
-  const itemCartTotal = document.querySelector('.itemCartTotal')
-  carrito.forEach((item) => {
-    const precio = Number(item.precio.replace("$", ''))
-    Total = Total + precio*item.cantidad
-  })
-
-  itemCartTotal.innerHTML = `Total $${Total}`
-  addLocalStorage()
-}
-
-function removeItemCarrito(e){
-  const buttonDelete = e.target
-  const tr = buttonDelete.closest(".ItemCarrito")
-  const title = tr.querySelector('.title').textContent;
-  for(let i=0; i<carrito.length ; i++){
-
-    if(carrito[i].title.trim() === title.trim()){
-      carrito.splice(i, 1)
+    const existe = articulosCarrito.some( ropa => ropa.id === infoRopa.id);
+    if (existe) {
+       
+        const ropa = articulosCarrito.map ( ropa => {
+            if (ropa.id === infoRopa.id) {
+                ropa.cantidad++;
+                return ropa;    
+            } else {
+                return ropa;    
+            }
+        } );
+        articulosCarrito = [...ropas];
+    } else {
+        articulosCarrito = [...articulosCarrito, infoRopa];
     }
-  }
 
-  const alert = document.querySelector('.remove')
 
-  setTimeout( function(){
-    alert.classList.add('remove')
-  }, 2000)
-    alert.classList.remove('remove')
+    console.log(articulosCarrito);
 
-  tr.remove()
-  CarritoTotal()
+    carritoHTML();
 }
 
-function sumaCantidad(e){
-  const sumaInput  = e.target
-  const tr = sumaInput.closest(".ItemCarrito")
-  const title = tr.querySelector('.title').textContent;
-  carrito.forEach(item => {
-    if(item.title.trim() === title){
-      sumaInput.value < 1 ?  (sumaInput.value = 1) : sumaInput.value;
-      item.cantidad = sumaInput.value;
-      CarritoTotal()
-    }
-  })
+//Muestra el carrito de compras en el HTML
+
+function carritoHTML() {
+
+    limpiarHTML();
+
+      articulosCarrito.forEach( (ropa) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>
+                 <img src="${ropa.imagen}" width="100" >
+            </td>
+            <td>
+                ${ropa.titulo}
+            </td>
+            <td>
+                ${ropa.precio}
+            </td>
+            <td>
+                ${ropa.cantidad}
+            </td>
+            <td>
+               <a href="#" class="borrar-ropa" data-id="${ropa.id}" > X </a>
+             </td>
+        `;
+
+        contenedorCarrito.appendChild(row)
+    });
+
+    sincronizarStorage();
+
 }
 
-function addLocalStorage(){
-  localStorage.setItem('carrito', JSON.stringify(carrito))
+function limpiarHTML() {
+    contenedorCarrito.innerHTML = "";
 }
 
-window.onload = function(){
-  const storage = JSON.parse(localStorage.getItem('carrito'));
-  if(storage){
-    carrito = storage;
-    renderCarrito()
-  }
+function sincronizarStorage() {
+    localStorage.setItem("carrito", JSON.stringify(articulosCarrito))
 }
